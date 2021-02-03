c9835ff14e7e314eb54c5bf30fff1619a70d39ce
iit2019017 <iit2019017@iiita.ac.in>
2021-02-03 13:12:51 +0530

    Added asg 2 folder

---
+++ ass2/Brut_force.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    int max_cnt=0,cnt=1;
+    cin>>n>>m;
+    int a[n][m];
+    for(int i=0;i<n;i++){
+    	for(int j=0;j<m;j++){
+    		cin>>a[i][j];
+		}
+	}
+    for(int i=0;i<n;i++){
+    	for(int j=1;j<m;j++)
+    	{
+    		if(a[i][j]>a[i][j-1])
+    		cnt++;
+    		else{
+    			max_cnt=max(cnt,max_cnt);
+    			cnt=0;
+			}
+		}
+	}
+	cout<<max_cnt;
+}
